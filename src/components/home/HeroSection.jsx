f058659f9a86d39d0e41b0f4ef273eb0ef8682b7
obfhub <arcadieaue@gmail.com>
2026-07-28 15:24:55 +0300

    adad

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -27,7 +27,7 @@ export default function HeroSection({ heroImage }) {
       </div>
 
       {/* Content Container - Left Aligned, Consistent Spacing Rhythm */}
-      <div className="relative z-10 w-full h-full flex flex-col items-start px-6 md:px-12 lg:px-16 pt-48 sm:pt-64 md:pt-80 lg:pt-[28rem] gap-6 md:gap-8">
+      <div className="relative z-10 w-full h-full flex flex-col items-start px-6 md:px-12 lg:px-16 pt-16 sm:pt-48 md:pt-80 lg:pt-[28rem] gap-6 md:gap-8">
 
         {/* Main Headlines - Left Aligned and Bold */}
         <motion.div
