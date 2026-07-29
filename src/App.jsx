aa0d40e41ffb6a843ce565f435397425ef58bc15
obfhub <arcadieaue@gmail.com>
2026-07-29 16:55:56 +0300

    Speed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import { LanguageProvider } from '@/lib/i18n/LanguageContext';
@@ -71,6 +72,7 @@ function App() {
           </Router>
           <Toaster />
           <Analytics />
+          <SpeedInsights />
         </QueryClientProvider>
       </LanguageProvider>
     </AuthProvider>
